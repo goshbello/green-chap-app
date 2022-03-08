9dc13b8f27cee6e18e0cc787fefe59dd408692f3
goshbello <44555654+goshbello@users.noreply.github.com>
2022-03-08 04:36:39 +0000

    add homepage and session controller for login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  root to: 'pages#index'
+  get 'login', to: 'sessions#new'
+
+
+
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
